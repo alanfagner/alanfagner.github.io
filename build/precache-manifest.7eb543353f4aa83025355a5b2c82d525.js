215cfa8c28f6460c2f68a06677b17ad81b849eb4
Alan Goncalves <alan.f.Goncalves@hotmai.com>
2019-05-05 21:24:58 +0100

    create build folder and generate release

---
+++ build/precache-manifest.7eb543353f4aa83025355a5b2c82d525.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "797bd37182c593f080682836cf1a730a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b38c087663a6e0ebddd",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "9b38c087663a6e0ebddd",
+    "url": "/static/js/2.a71a2b13.chunk.js"
+  },
+  {
+    "revision": "718040aa53611d24909b",
+    "url": "/static/js/main.3af26eaf.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "7864e73075a0a05a4b58b7adf731cda4",
+    "url": "/static/media/Angularjs-icon.7864e730.svg"
+  },
+  {
+    "revision": "2ec0d5f78cc10a64b267db9371c7b500",
+    "url": "/static/media/Java-icon.2ec0d5f7.svg"
+  },
+  {
+    "revision": "cc2b3aafefa9bd0890b1fcc6b9ca835f",
+    "url": "/static/media/Nodejs-icon.cc2b3aaf.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/static/media/React-icon.05a3e30f.svg"
+  },
+  {
+    "revision": "e927a87850ed0ebc0078aabe02b0d429",
+    "url": "/static/media/background.e927a878.jpg"
+  }
+]);
